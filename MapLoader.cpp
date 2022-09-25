5ec6f0c9d82060a59369df351fa448d4605bb3ac
chongsuk21 <chongsuk21@gmail.com>
2022-09-25 00:03:04 -0400

    first day2

--- MapLoader.cpp
+++ MapLoader.cpp
@@ -1,1 +1,18 @@
 //MapLoader.cpp
+#include <iostream>
+#include <vector>
+#include "Territory.h"
+
+using namespace std;
+
+    //vector<int> v1 = {1,2,3,4};
+    //v1.insert(v1.begin() , 5);
+    //cout << v1[0] << endl;
+
+void loadMap(){
+    //territory --> pointer
+    // a group of territories --> vector
+    
+}
+
+
