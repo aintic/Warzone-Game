836c8f83b2c7fe2507f9e14f940e2648271b1b76
dashax <dariadenejkina@gmail.com>
2022-10-05 11:29:11 -0400

    GameEngine almost done

--- GameEngine/GameEngineDriver.cpp
+++ GameEngine/GameEngineDriver.cpp
@@ -1,3 +1,30 @@
-#pragma once
+#include <iostream>
+#include <string>
+#include "GameEngine.h"
+using std::cout;
+using std::cin;
+using std::string;
 
-#include "GameEngineDriver.h"
+
+void testGameStates(){
+
+    GameEngine* game;
+    game = new GameEngine();
+    string command;
+    cout << "\nGame begins in state: ";
+    cout << game->getCurrentState()->getStateName() << "\n";
+
+    do{
+        cout << "\nPlease enter command to move to the next state: \n";
+        cin >> command;
+        game->getCurrentState()->transition(game, command);
+    }while(true);
+
+}
+
+
+
+int main()
+{
+    testGameStates();
+}
